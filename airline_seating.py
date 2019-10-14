b3ea5e87aef7f7cf17f67e2750d5126c641dbd9b
madsaken <haffisaken@gmail.com>
2019-10-14 12:57:33 +0000

    Nærstum tilbúið

--- airline_seating.py
+++ airline_seating.py
@@ -8,20 +8,22 @@ def inputs():
     #sel_seat = input("Input seat number (row seat): ")
     return row, seats  
 
+
 def make_rows(row, seats):
     
     '''This makes the big list with the seats'''
     
     big_list = []
     seat_list = [] 
-    counter = 0
     char = list(string.ascii_uppercase)
-    for i in range(0, seats):
-        seat_list.append(char[counter])
-        counter += 1
-    for i in range(0, row):
+    
+    for _ in range(0, row):
+        for i in range(0, seats):
+            seat_list.append(char[i])
         big_list.append(seat_list)
+        seat_list = [] 
     return big_list
+
 
 def select_seats(big_list):
     while True:
@@ -38,15 +40,25 @@ def select_seats(big_list):
         errcheck = False
         for rowl in range(len(big_list)):
             for i, iteml in enumerate(big_list[rowl]):
-                if (row-1 == rowl) and (seat == iteml):
+                if (row-1 == rowl) and ((seat == iteml) or (iteml == "X")):
                     if big_list[rowl][i] == "X":
                         errcheck = True
                         print("That seat is taken!")
-                    else:    
+                    else:
                         big_list[rowl][i] = "X"
                         return big_list
+
         if errcheck == False:
             print("Seat number is invalid!")
+
+
+def again():
+    ans = input("More seats (y/n)? ")
+    if ans == "y":
+        return True
+    else:
+        return False
+
             
 def printout(big_list):
     
@@ -54,15 +66,20 @@ def printout(big_list):
     
     print(big_list)
 
+
 def main():
+    moreseats = True
+
     row, seats = inputs()
     
     big_list = make_rows(row, seats)
     
     printout(big_list)
     
-    select_seats(big_list)
-    
-    printout(big_list)
+    while moreseats:
+        select_seats(big_list)
+        printout(big_list)
+        moreseats = again()
+
     
 main()
